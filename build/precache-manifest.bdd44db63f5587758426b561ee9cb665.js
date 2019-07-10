f04e94755a5b094ef218b8e3645b5b0cfdd01375
prerna <chaturvediprerna03@gmail.com>
2019-07-10 15:17:17 +1200

    build

---
+++ build/precache-manifest.bdd44db63f5587758426b561ee9cb665.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb90a7b099b056280a4233827319dbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dccb2dd37f4c8dc6c0d",
+    "url": "/static/css/main.8fd1bf4c.chunk.css"
+  },
+  {
+    "revision": "420ba1678d3cd1a3d00d",
+    "url": "/static/js/2.478073eb.chunk.js"
+  },
+  {
+    "revision": "8dccb2dd37f4c8dc6c0d",
+    "url": "/static/js/main.260fa9e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
